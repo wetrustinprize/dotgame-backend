5cb4248f95f0df3e479f4c1d17aa1edf64cd81a5
Peterson Adami Candido <contact.petersonac@protonmail.com>
2022-05-03 16:59:58 -0300

    get the logged user

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,6 +1,9 @@
+import { User } from '@generated/prisma/user/models/user.model';
+import { UseGuards } from '@nestjs/common';
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CurrentUser } from 'auth/current-user.decorator';
+import { LoggedGuard } from 'auth/logged.guard';
 import { CreateUserDTO } from './dtos/user-create.dto';
-import { User } from './models/user.model';
 import { UsersService } from './users.service';
 
 @Resolver(() => User)
@@ -15,5 +18,11 @@ export class UsersResolver {
   @Query(() => User)
   async getUserById(@Args('id') id: string): Promise<User> {
     return (await this.usersService.findById(id)) as User;
+  }
+
+  @Query(() => User)
+  @UseGuards(LoggedGuard)
+  async getLoggedUser(@CurrentUser() user: User): Promise<User> {
+    return user;
   }
 }
