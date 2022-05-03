ad232f099f283a0cbb1ae2e9b2ad286ccdf2e9c3
Peterson Adami Candido <contact.petersonac@protonmail.com>
2022-05-03 17:00:13 -0300

    config module and graphql context

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,29 @@
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+
 import { GamesModule } from './games/games.module';
 import { UsersModule } from './users/users.module';
 
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from 'auth/auth.module';
+
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
     GamesModule,
     UsersModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
+      context: ({ req }) => {
+        req;
+      },
     }),
   ],
 })
