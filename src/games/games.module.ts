3b076c04c2b916cbd5f913719357a3a5036da3f4
Peterson Adami Candido <contact.petersonac@protonmail.com>
2022-05-03 21:56:57 -0300

    create game and draw line methods

--- src/games/games.module.ts
+++ src/games/games.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'prisma.service';
 import { GamesResolver } from './games.resolver';
 import { GamesService } from './games.service';
 
 @Module({
-  providers: [GamesService, GamesResolver],
+  providers: [GamesService, GamesResolver, PrismaService],
 })
 export class GamesModule {}
--- src/games/games.resolver.ts
+++ src/games/games.resolver.ts
@@ -1,10 +1,32 @@
-import { Query, Resolver } from '@nestjs/graphql';
-import { Game } from './models/game.model';
-
+import { Game } from '@generated/prisma/game/models/game.model';
+import { User } from '@generated/prisma/user/models/user.model';
+import { UseGuards } from '@nestjs/common';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CurrentUser } from 'auth/current-user.decorator';
+import { LoggedGuard } from 'auth/logged.guard';
+import { CreateBoardDTO } from './dtos/create-board.dto';
+import { DrawLineDTO } from './dtos/draw-line.dto';
+import { GamesService } from './games.service';
 @Resolver(() => Game)
 export class GamesResolver {
-  @Query(() => String)
-  sayHello(): string {
-    return 'Hello world!';
+  constructor(private readonly gamesService: GamesService) {}
+
+  @Mutation(() => Game)
+  @UseGuards(LoggedGuard)
+  async createGame(
+    @CurrentUser() user: User,
+    @Args() createBoardDTO: CreateBoardDTO,
+  ): Promise<Game> {
+    return this.gamesService.createGame(user, createBoardDTO);
+  }
+
+  @Mutation(() => Game)
+  @UseGuards(LoggedGuard)
+  async drawLine(
+    @CurrentUser() user: User,
+    @Args('id') gameId: string,
+    @Args() drawLineDTO: DrawLineDTO,
+  ): Promise<Game> {
+    return this.gamesService.drawLine(user, gameId, drawLineDTO);
   }
 }
